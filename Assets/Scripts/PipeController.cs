bd47709295b74c4f02a01d292da31112da76b377
Victor Bonnier <Vikkear@hotmail.com>
2020-06-11 01:15:24 +0200

    FirstCommit

---
+++ Assets/Scripts/PipeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeController : MonoBehaviour
+{
+    public Rigidbody2D pipeBody;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // Game over
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 1.5f;
+
+    public float gravity = 1f;
+
+    public float gravityScale = 0.05f;
+
+    public float gravityReset = -5f;
+
+    public float maxSpeed = -8f;
+
+    public Rigidbody2D playerBody;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            gravity += gravityReset;
+            if (gravity < maxSpeed)
+            {
+                gravity = maxSpeed;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        gravity += gravityScale;
+        playerBody.MovePosition(playerBody.position + new Vector2(1 * movementSpeed, -1 * gravity) * Time.fixedDeltaTime);
+    }
+}
